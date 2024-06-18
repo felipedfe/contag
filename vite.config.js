46b12eb967eca0e879013af24fb394d2233fc900
Felipe Fernandes <felipedfe@gmail.com>
2024-06-18 17:42:45 -0300

    change vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     host: '0.0.0.0'
-//   }
-// })
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: '0.0.0.0'
+  }
+})
 
 
 
@@ -30,14 +30,14 @@
 //     }
 //   });
 
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: '/seraquevaichover/', 
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-  }
-});
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   base: '/seraquevaichover/', 
+//   plugins: [react()],
+//   server: {
+//     host: '0.0.0.0',
+//   }
+// });
